aa879a8791e907a242bf11cb5f69e56382b4fa89
malugoms <158211763+malugoms@users.noreply.github.com>
2024-02-01 16:32:53 -0300

    Create terminar.java

---
+++ .vscode/terminar.java
@@ -0,0 +1,32 @@
+package teste;
+
+class Quadrado{
+private int lado;
+
+public Quadrado(int lado){
+    this.lado = lado;
+}
+
+public int getLado(){
+    return lado;
+}
+
+public void setLado(int lado) {
+    this.lado = lado;
+}
+
+public double calcularArea() {
+    return lado*lado;
+}
+
+public static void main(int[] args) {
+    //Teste da classe Quadrado
+
+Quadrado meuQuadrado = new Quadrado(lado:12);
+System.out.println("")
+
+}
+}
+   
+    
+}
